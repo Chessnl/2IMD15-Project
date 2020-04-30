712b97bd7d0177a6daa8b240caa16badcd8f50e5
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2020-04-30 23:50:56 +0200

    Implemented getAverage function

--- src/main/java/Correlations/PearsonCorrelation.java
+++ src/main/java/Correlations/PearsonCorrelation.java
@@ -9,6 +9,22 @@ public class PearsonCorrelation implements CorrelationFunction {
     @Override
     public double getCorrelation(List<Tuple2<Date, Double>> first, List<Tuple2<Date, Double>> second) {
         // @TODO Implement
+
+
+
         return 0;
     }
+
+    private double getAverage(List<Tuple2<Date, Double>> data){
+        double sum = 0;
+        if(!data.isEmpty()) {
+            for (Tuple2<Date, Double> point: data) {
+                sum += point._2;
+            }
+            return sum / data.size();
+        }
+        return 0;
+    }
+
+
 }
