cf1e71448da3003fd368714668e8ac26802e06da
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2020-05-01 00:04:35 +0200

    Implemented getCorrelation function

--- src/main/java/Correlations/PearsonCorrelation.java
+++ src/main/java/Correlations/PearsonCorrelation.java
@@ -3,16 +3,39 @@ package Correlations;
 import scala.Tuple2;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.lang.Math;
 
 public class PearsonCorrelation implements CorrelationFunction {
     @Override
     public double getCorrelation(List<Tuple2<Date, Double>> first, List<Tuple2<Date, Double>> second) {
-        // @TODO Implement
 
+        // Retrieve averages
+        double avgX = getAverage(first);
+        double avgY = getAverage(second);
 
+        // Prepare for simultaneous looping
+        double cov = 0;
+        double stdX = 0;
+        double stdY = 0;
+        Iterator<Tuple2<Date, Double>> iterX = first.iterator();
+        Iterator<Tuple2<Date, Double>> iterY = second.iterator();
 
-        return 0;
+        // Process for all elements in a simultaneous loop
+        while (iterX.hasNext() && iterY.hasNext()){
+            double xi = iterX.next()._2;
+            double yi = iterY.next()._2;
+
+            cov += (xi-avgX)*(yi-avgY);
+            stdX += Math.pow((xi-avgX), 2);
+            stdY += Math.pow((yi-avgY), 2);
+        }
+
+        // Solve final equation
+        stdX = Math.sqrt(stdX);
+        stdY = Math.sqrt(stdY);
+        return cov/(stdX*stdY);
     }
 
     private double getAverage(List<Tuple2<Date, Double>> data){
