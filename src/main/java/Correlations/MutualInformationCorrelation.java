19db36b308bc14bcbf9771fdf872510a1c8c4895
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2020-05-01 13:08:43 +0200

    Fixed issue with XY histogram

--- src/main/java/Correlations/MutualInformationCorrelation.java
+++ src/main/java/Correlations/MutualInformationCorrelation.java
@@ -5,6 +5,7 @@ import scala.Serializable;
 import scala.Tuple2;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 public class MutualInformationCorrelation implements CorrelationFunction, Serializable {
@@ -13,11 +14,41 @@ public class MutualInformationCorrelation implements CorrelationFunction, Serial
     public double getCorrelation(List<Tuple2<Date, Double>> first, List<Tuple2<Date, Double>> second) {
         int nrBuckets = 20;
 
-        // Prepare histograms
-        Pair<Double, Double> minMax = findGlobalMinMax(first, second);
-        double[] histX = createHist1D(first, nrBuckets, minMax.getKey(), minMax.getValue());
-        double[] histY = createHist1D(second, nrBuckets, minMax.getKey(), minMax.getValue());
-        double[][] histXY = createHist2D(first, second, nrBuckets, minMax.getKey(), minMax.getValue());
+        // Determine min and max
+        Pair<Double, Double> minMaxX = findMinMax(first);
+        Pair<Double, Double> minMaxY = findMinMax(second);
+
+        // Prepare equi-width histograms
+        double[] histX = new double[nrBuckets];
+        double[] histY = new double[nrBuckets];
+        double[][] histXY = new double[nrBuckets][nrBuckets];
+
+        // Determine steps
+        double stepX = minMaxX.getKey() / minMaxX.getValue();
+        double stepY = minMaxY.getKey() / minMaxY.getValue();
+        double singleIncr = 1.0/first.size();
+        double doubleIncr = 1.0/(first.size()*second.size());
+
+        // Prepare for simultaneous looping
+        Iterator<Tuple2<Date, Double>> iterX = first.iterator();
+        Iterator<Tuple2<Date, Double>> iterY = second.iterator();
+
+        if(!first.isEmpty() && !second.isEmpty()) {
+
+            // Loop over both timeseries
+            while (iterX.hasNext() && iterY.hasNext()){
+                double xi = iterX.next()._2;
+                double yi = iterY.next()._2;
+
+                // Determine both buckets
+                int bucketX = (int) ((xi-minMaxX.getKey()) / stepX);
+                int bucketY = (int) ((yi-minMaxY.getKey()) / stepY);
+
+                histX[bucketX] += singleIncr;
+                histY[bucketY] += singleIncr;
+                histXY[bucketX][bucketY] += doubleIncr;
+            }
+        }
 
         // Calculate for all histogram bucket combinations
         double MI = 0;
@@ -28,61 +59,6 @@ public class MutualInformationCorrelation implements CorrelationFunction, Serial
         }
 
         return MI;
-    }
-
-    // Create a 1D histogram to approximate a discrete probability distribution function
-    private double[] createHist1D(List<Tuple2<Date, Double>> data, int nrBuckets, double min, double max){
-
-        // Step info
-        double step = (max-min)/nrBuckets;          // The size of one bucket
-        double[] hist = new double[nrBuckets];      // The actual equi-width histogram
-        double increment = 1.0 / data.size();       // 1/data.size() for each point sums to 1.
-
-        if(!data.isEmpty()) {
-
-            // Increment the correct buckets
-            for (Tuple2<Date, Double> point: data) {
-                int bucket = (int) ((point._2-min) / step);
-                hist[bucket] += increment;
-            }
-        }
-
-        return hist;
-    }
-
-    // Create a 2D histogram to approximate a discrete probability distribution function
-    private double[][] createHist2D(List<Tuple2<Date, Double>> dataX, List<Tuple2<Date, Double>> dataY,
-                                    int nrBuckets, double min, double max){
-
-        double step = (max-min)/nrBuckets;
-        double[][] hist = new double[nrBuckets][nrBuckets];
-        double increment = 1.0 / (dataX.size()*dataY.size());   //X*Y sums add up to one, satisfying probality dist.
-
-        if(!dataX.isEmpty() && !dataY.isEmpty()) {
-
-            // Loop over both timeseries
-            for (Tuple2<Date, Double> pointX: dataX) {
-                for (Tuple2<Date, Double> pointY: dataY){
-
-                    // Determine both buckets
-                    int bucketX = (int) ((pointX._2-min) / step);
-                    int bucketY = (int) ((pointY._2-min) / step);
-
-                    hist[bucketX][bucketY] += increment;
-                }
-            }
-        }
-
-        return hist;
-    }
-
-    // Finds the ultimate minimum and maximum of two datasets
-    private Pair<Double, Double> findGlobalMinMax(List<Tuple2<Date, Double>> dataX, List<Tuple2<Date, Double>> dataY){
-        Pair<Double, Double> minMaxX = findMinMax(dataX);
-        Pair<Double, Double> minMaxY = findMinMax(dataY);
-
-        return new Pair<>(  Math.min(minMaxX.getKey(), minMaxY.getKey()),
-                            Math.max(minMaxX.getValue(), minMaxY.getValue()));
     }
 
     // Find the minimum and maximum of a single dataset
