e7736e9caffeb1fe51cf34646a0a3920871f80f3
Wouter <w.j.g.m.v.d.bemd@student.tue.nl>
2020-05-15 12:40:42 +0200

    Added a 2D test case

--- src/main/java/test/TotalCorrelationTest.java
+++ src/main/java/test/TotalCorrelationTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import Correlations.CorrelationFunction;
-import Correlations.MutualInformationCorrelation;
 import Correlations.TotalCorrelation;
 import org.junit.jupiter.api.Test;
 import scala.Tuple2;
@@ -12,8 +11,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TotalCorrelationTest {
-
-    private final CorrelationFunction correl = new MutualInformationCorrelation();
 
     @Test
     void test1(){
@@ -38,5 +35,41 @@ class TotalCorrelationTest {
         }}));
 
         System.out.print(new TotalCorrelation().getCorrelation(stocks));
+    }
+
+    @Test
+    void test2(){
+        List<Tuple2<String, List<Double>>> stocks = new ArrayList<>();
+
+        stocks.add(new Tuple2<>("Stock1", new LinkedList<Double>(){
+            {
+                add(1.0);
+                add(1.0);
+                add(1.0);
+                add(1.0);
+                add(2.0);
+                add(2.0);
+                add(2.0);
+                add(2.0);
+                add(2.0);
+            }
+        }));
+
+        stocks.add(new Tuple2<>("Stock1", new LinkedList<Double>(){
+            {
+                add(1.0);
+                add(1.0);
+                add(2.0);
+                add(2.0);
+                add(1.0);
+                add(1.0);
+                add(1.0);
+                add(1.0);
+                add(2.0);
+            }
+        }));
+
+        double error = Math.abs(new TotalCorrelation().getCorrelation(stocks)-0.050447408);
+        assertTrue(error < 0.01);
     }
 }
