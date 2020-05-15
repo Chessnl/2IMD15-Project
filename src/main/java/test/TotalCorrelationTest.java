375bd3c46d8eaf98d377abd9d789d5415ec8a469
Chess <34745596+Chessnl@users.noreply.github.com>
2020-05-15 12:54:36 +0200

    Updated test cases

--- src/main/java/test/TotalCorrelationTest.java
+++ src/main/java/test/TotalCorrelationTest.java
@@ -13,30 +13,51 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TotalCorrelationTest {
 
-    private final CorrelationFunction correl = new MutualInformationCorrelation();
+    private final TotalCorrelation correl = new TotalCorrelation();
 
     @Test
     void test1(){
         List<Tuple2<String, List<Double>>> stocks = new ArrayList<>();
 
         stocks.add(new Tuple2<>("Stock1", new LinkedList<Double>(){{
-                add(1.0);
-                add(2.0);
-                add(0.5);
-                add(1.0);
-                add(1.0);
-                add(2.0);
+            add(0.0);
+            add(1.0);
         }}));
 
         stocks.add(new Tuple2<>("Stock2", new LinkedList<Double>(){{
-                add(1.0);
-                add(1.0);
-                add(1.0);
-                add(2.0);
-                add(2.0);
-                add(2.0);
+            add(0.0);
+            add(1.0);
         }}));
 
-        System.out.print(new TotalCorrelation().getCorrelation(stocks));
+        double error = Math.abs(correl.getCorrelation(stocks)-Math.log(2));
+        assertTrue(error < 0.01);
+    }
+
+    @Test
+    void test2(){
+        List<Tuple2<String, List<Double>>> stocks = new ArrayList<>();
+
+        stocks.add(new Tuple2<>("Stock1", new LinkedList<Double>(){{
+            add(0.0);
+            add(1.0);
+        }}));
+
+        stocks.add(new Tuple2<>("Stock2", new LinkedList<Double>(){{
+            add(0.0);
+            add(1.0);
+        }}));
+
+        stocks.add(new Tuple2<>("Stock3", new LinkedList<Double>(){{
+            add(0.0);
+            add(1.0);
+        }}));
+
+        stocks.add(new Tuple2<>("Stock4", new LinkedList<Double>(){{
+            add(0.0);
+            add(1.0);
+        }}));
+
+        double error = Math.abs(correl.getCorrelation(stocks)-Math.log(8));
+        assertTrue(error < 0.01);
     }
 }
