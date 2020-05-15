8620b788215d4d05240d8e23c7725149fa5c18ff
Chess <34745596+Chessnl@users.noreply.github.com>
2020-05-15 13:16:40 +0200

    Added test for associativity

--- src/main/java/test/TotalCorrelationTest.java
+++ src/main/java/test/TotalCorrelationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -57,5 +58,45 @@ class TotalCorrelationTest {
 
         double error = Math.abs(correl.getCorrelation(stocks)-Math.log(8));
         assertTrue(error < 0.01);
+    }
+
+    @Test // test for associativity
+    void test3() {
+        List<Tuple2<String, List<Double>>> stocks = new ArrayList<>();
+
+        stocks.add(new Tuple2<>("Stock1", new LinkedList<Double>(){{
+            add(10.0);
+            add(4.0);
+            add(7.0);
+            add(2.0);
+            add(6.0);
+            add(8.0);
+        }}));
+
+        stocks.add(new Tuple2<>("Stock2", new LinkedList<Double>(){{
+            add(1.0);
+            add(3.0);
+            add(7.0);
+            add(4.0);
+            add(9.0);
+            add(10.0);
+        }}));
+
+        stocks.add(new Tuple2<>("Stock3", new LinkedList<Double>(){{
+            add(4.0);
+            add(1.0);
+            add(3.0);
+            add(3.0);
+            add(6.0);
+            add(3.0);
+        }}));
+
+        double corr1 = correl.getCorrelation(stocks);
+        Collections.shuffle(stocks);
+        double corr2 = correl.getCorrelation(stocks);
+        Collections.shuffle(stocks);
+        double corr3 = correl.getCorrelation(stocks);
+
+        assertTrue(Math.abs(corr1 - corr2) < 0.01 && Math.abs(corr2 - corr3) < 0.01);
     }
 }
