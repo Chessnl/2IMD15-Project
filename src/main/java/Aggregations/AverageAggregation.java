258ecbd0910c147a9c19de515f5f7ff8392354ea
Square Play'n <pietervoors@hotmail.com>
2020-05-29 12:23:21 +0200

    Fix name concat's trailing + in dimensionality-reducing aggregators

--- src/main/java/Aggregations/AverageAggregation.java
+++ src/main/java/Aggregations/AverageAggregation.java
@@ -5,6 +5,7 @@ import scala.Tuple2;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AverageAggregation extends AggregationFunction {
 
@@ -18,18 +19,16 @@ public class AverageAggregation extends AggregationFunction {
 
     @Override
     public List<Tuple2<String, List<Double>>> singleAggregation(List<Tuple2<String, List<Double>>> in) {
-        StringBuilder stockname = new StringBuilder();
+        String stockName = in.stream().map(stock -> stock._1).collect(Collectors.joining("+"));
         Double[] prices = new Double[in.get(0)._2.size()];
         Arrays.fill(prices, 0.);
 
         for (Tuple2<String, List<Double>> stock : in) {
-            stockname.append("(").append(stock._1).append(") + ");
-
             for (int i = 0; i < prices.length; i++) {
                 prices[i] += stock._2.get(i) / prices.length;
             }
         }
 
-        return Collections.singletonList(new Tuple2<>(stockname.toString(), Arrays.asList(prices)));
+        return Collections.singletonList(new Tuple2<>(stockName, Arrays.asList(prices)));
     }
 }
--- src/main/java/Aggregations/MaxAggregation.java
+++ src/main/java/Aggregations/MaxAggregation.java
@@ -5,6 +5,7 @@ import scala.Tuple2;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MaxAggregation extends AggregationFunction {
 
@@ -18,18 +19,17 @@ public class MaxAggregation extends AggregationFunction {
 
     @Override
     public List<Tuple2<String, List<Double>>> singleAggregation(List<Tuple2<String, List<Double>>> in) {
-        StringBuilder stockname = new StringBuilder();
+        String stockName = in.stream().map(stock -> stock._1).collect(Collectors.joining("+"));
         Double[] prices = new Double[in.get(0)._2.size()];
         Arrays.fill(prices, Double.MIN_VALUE);
 
         for (Tuple2<String, List<Double>> stock : in) {
-            stockname.append("(").append(stock._1).append(") + ");
             for (int i = 0; i < prices.length; i++) {
                 prices[i] = Math.max(prices[i], stock._2.get(i));
             }
         }
 
-        return Collections.singletonList(new Tuple2<>(stockname.toString(), Arrays.asList(prices)));
+        return Collections.singletonList(new Tuple2<>(stockName, Arrays.asList(prices)));
     }
 
 }
